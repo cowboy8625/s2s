6a8e17e12e5915200326bff9acacf3f00781a6b6
cowboy8625 <cowboy8625@yahoo.com>
2024-10-27 09:56:15 -0500

    usable with .zon

--- build.zig
+++ build.zig
@@ -1,20 +1,32 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("s2s", .{
-        .root_source_file = .{ .path = "s2s.zig" },
-    });
-
-    const tests = b.addTest(.{
-        .root_source_file = .{ .path = "s2s.zig" },
+    const lib = b.addStaticLibrary(.{
+        .name = "s2s",
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
+    b.installArtifact(lib);
+
+    _ = b.addModule("s2s", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const lib_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
     const test_step = b.step("test", "Run unit tests");
-    const run_test = b.addRunArtifact(tests);
-    test_step.dependOn(&run_test.step);
+    test_step.dependOn(&run_lib_unit_tests.step);
 }
